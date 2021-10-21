363fa84c65d096aa82fc6deb870f3cb45f136ff1
BJudge <bartjudge@gmail.com>
2021-10-22 00:29:00 +0100

    checking the webhook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -49,7 +49,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_refeence_id;
   const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.object.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -65,7 +65,7 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
-  if (event.type === 'checkout.session.complete') {
+  if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
     res.status(200).json({ recieved: true });
   }
