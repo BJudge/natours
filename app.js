a93d3c9a8eb33ec4d91a6ae28b308a612bff3b3b
BJudge <bartjudge@gmail.com>
2021-10-21 23:36:18 +0100

    ammneded app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -8,160 +7,108 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 
 const AppError = require('./utils/appError');
-
 const globalErrorHandler = require('./controllers/errorController');
-
-const tourRouter = require(`./routes/tourRoutes`);
-const userRouter = require(`./routes/userRoutes`);
-const reviewRouter = require(`./routes/reviewRoutes`);
-const bookingRouter = require(`./routes/bookingRoutes`);
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const bookingController = require('./controllers/bookingController');
-const viewRouter = require(`./routes/viewRoutes`);
+const viewRouter = require('./routes/viewRoutes');
 
+// Start express app
 const app = express();
+
 app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
 app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
 
 app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
 app.post(
   '/webhook-checkout',
-  express.raw({ type: 'application/json' }),
+  bodyParser.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
 
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
+// Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
+// Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
 app.use(
   hpp({
     whitelist: [
       'duration',
       'ratingsQuantity',
       'ratingsAverage',
-      'maxGroupSie',
+      'maxGroupSize',
       'difficulty',
       'price',
     ],
   })
 );
 
-app.use(express.static(path.join(__dirname, `/public`)));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: [
-          "'self'",
-          'data:',
-          'blob:',
-          'https:',
-          'ws:',
-          'wss:',
-          'https://travel-site-app.herokuapp.com:*',
-          'https://travel-site-app.herokuapp.com/',
-          'http://127.0.0.1:3000/api/v1/booking/checkout-session/*',
-        ],
-        baseUri: ["'self'"],
-        fontSrc: ["'self'", 'https:', 'data:'],
-        scriptSrc: [
-          "'self'",
-          'wss:',
-          'https:',
-          'http:',
-          'blob:',
-          'https://*.mapbox.com',
-          'https://js.stripe.com/v3/',
-          'https://m.stripe.network',
-          'https://*.cloudflare.com',
-          'https://travel-site-app.herokuapp.com/*',
-          'https://travel-site-app.herokuapp.com:*',
-        ],
-        frameSrc: ["'self'", 'https://js.stripe.com/v3/'],
-        objectSrc: ["'none'"],
-        styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
-        workerSrc: [
-          "'self'",
-          'data:',
-          'blob:',
-          'https://*.tiles.mapbox.com',
-          'https://api.mapbox.com',
-          'https://events.mapbox.com',
-          'https://m.stripe.network',
-        ],
-        childSrc: [
-          "'self'",
-          'blob:',
-          'wss',
-          'ws',
-          'https://travel-site-app.herokuapp.com/*',
-        ],
-        imgSrc: ["'self'", 'data:', 'blob:'],
-        formAction: ["'self'"],
-        connectSrc: [
-          "'self'",
-          "'unsafe-inline'",
-          'data:',
-          'blob:',
-          'wss:',
-          'ws',
-          'https://*.stripe.com',
-          'https://*.mapbox.com',
-          'https://*.cloudflare.com/',
-          'https://bundle.js:*',
-          'wss://travel-site-app.herokuapp.com:*',
-          'wss://travel-site-app.herokuapp.com:53552/',
-          'https://127.0.0.1:3000/api/v1/users/login',
-          'http://127.0.0.1:3000/api/v1/users/login',
-          'ws://127.0.0.1:*',
-        ],
-        upgradeInsecureRequests: [],
-      },
-    },
-  })
-);
-app.use((req, res, next) => {
-  next();
-});
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour',
-});
-app.use('/api', limiter);
-
 app.use(compression());
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
   next();
 });
 
-//mount routers
-
+// 3) ROUTES
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
-app.use('/api/v1/booking', bookingRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
