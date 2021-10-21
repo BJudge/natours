0797659cf1269c4dd1e2857cdf26c17f1d202c38
BJudge <bartjudge@gmail.com>
2021-10-21 08:37:09 +0100

    changing PORT env

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
     console.log('DB connection successful!');
   });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 console.log(process.env.NODE_ENV);
 
